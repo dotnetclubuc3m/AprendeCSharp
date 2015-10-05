18067c7e226741e7875dcc9bbf5db0643ffee4bd
kikoncuo <enriqueag26@gmail.com>
2015-10-05 18:49:21 +0200

    Estructurando 1

---
+++ 2  -Bola Magica/Inicio/Bola Magica-inicio/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bola_Magica_inicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
